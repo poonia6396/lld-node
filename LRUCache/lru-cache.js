119e6e7e913ca64f53894198deaf7dea1607d5e9
poonia6396 <nishant.poonia6396@gmail.com>
2026-05-21 11:22:30 +0530

    add lru-cache

---
+++ LRUCache/lru-cache.js
@@ -0,0 +1,79 @@
+class Node {
+    next;
+    prev;
+
+    constructor(key, val) {
+        this.key = key;
+        this.val = val;
+    }
+}
+
+class LRUCache {
+
+    #capacity;
+    #dll_head;
+    #dll_tail;
+    #map;
+
+
+    constructor(capacity) {
+        this.#capacity = capacity;
+        this.#dll_head = new Node();
+        this.#dll_tail = new Node();
+        this.#dll_head.next = this.#dll_tail;
+        this.#dll_tail.prev = this.#dll_head;
+        this.#map = new Map();
+    }
+
+    get(key) {
+
+        if(this.#map.has(key)) {
+            const result_node = this.#map.get(key);
+            this.#removeNode(result_node);
+            this.#addToFront(result_node);
+            return result_node.val;
+        }
+
+        return null;
+    }
+
+    put(key, val) {
+        
+        const node = this.#map.get(key) || new Node(key, val);
+        
+        if(this.#map.has(key)) {
+            node.val = val;
+            this.#removeNode(node);
+        }
+        else {
+            if(this.#map.size == this.#capacity){
+                this.#evict();
+            }
+        }
+
+        this.#addToFront(node);
+        this.#map.set(key, node);
+    }
+
+    #addToFront(node){
+        const tmp = this.#dll_head.next;
+        this.#dll_head.next = node;
+        node.next = tmp;
+        tmp.prev = node;
+        node.prev = this.#dll_head;
+    }
+
+    #removeNode(node) {
+        const prev = node.prev;
+        const next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    #evict() {
+        const lru_node = this.#dll_tail.prev;
+        this.#removeNode(lru_node);
+        this.#map.delete(lru_node.key);
+    }
+
+}
